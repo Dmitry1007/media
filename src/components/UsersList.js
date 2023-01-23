8d6d3d94d2a083b60efda341316ce8344949ccb7
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-23 14:03:09 -0700

    Display correct loading state while fetching users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,16 +1,28 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 function UsersList() {
   const dispatch = useDispatch();
+
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
 
   useEffect(() => {
     dispatch(fetchUsers());
     // erroneous eslint package error, makes us add [dispatch] for it to go away
   }, [dispatch]);
 
-  return "Users List";
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
 
 export default UsersList;
