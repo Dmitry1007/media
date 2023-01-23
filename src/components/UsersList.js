537d761f9ddc931df469083741a9db3fd018688e
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-23 13:54:31 -0700

    fetch users from the local db upon first render

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
 function UsersList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+    // erroneous eslint package error, makes us add [dispatch] for it to go away
+  }, [dispatch]);
+
   return "Users List";
 }
 
