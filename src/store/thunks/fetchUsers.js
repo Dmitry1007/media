7bbcbba5050fcd31f90a161f8cd9eaa720b1f6fb
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-23 17:35:37 -0700

    Hardcode a 2 second pause while fetching users

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,7 +3,16 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
+  // Development Only!
+  await pause(2000);
   return response.data;
 });
+
+// Development Only!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
