d32e3a64a8d4acd66555738af1f1f6e89706977b
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-26 13:11:36 -0700

    Clicking on trashcan icon next to user, deletes user from db and store

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { fetchUsers, addUser } from "../store";
+import { fetchUsers, addUser, removeUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 import { useThunk } from "../hooks/use-thunk";
+import { GoTrashcan } from "react-icons/go";
 
 function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
     useThunk(fetchUsers);
   const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+  const [doDeleteUser, isDeletingUser, deletingUserError] =
+    useThunk(removeUser);
 
   const { data } = useSelector((state) => {
     return state.users;
@@ -22,10 +25,21 @@ function UsersList() {
     doCreateUser();
   };
 
+  const handleUserRemove = (user) => {
+    doDeleteUser(user);
+  };
+
   const renderedUsers = data.map((user) => {
     return (
       <div key={user.id} className="mb-2 border rounded">
         <div className="flex p-2 justify-between items-center cursor-pointer">
+          <Button
+            loading={isDeletingUser}
+            onClick={() => handleUserRemove(user)}
+          >
+            <GoTrashcan />
+          </Button>
+          {deletingUserError && <div>Error Deleting User!</div>}
           {user.name}
         </div>
       </div>
