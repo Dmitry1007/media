713d296a320ab0cfcba9c72f85c73f066ad967a8
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-30 13:24:16 -0700

    Better way to manage tags

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -16,7 +16,12 @@ const albumsApi = createApi({
     return {
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          const tags = result.map((album) => {
+            return { type: "Album", id: album.id };
+          });
+          tags.push({ type: "UsersAlbums", id: user.id });
+          console.log(`Tags ${tags}`);
+          return tags;
         },
         query: (user) => {
           return {
@@ -30,7 +35,7 @@ const albumsApi = createApi({
       }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          return [{ type: "UsersAlbums", id: user.id }];
         },
         query: (user) => {
           return {
@@ -42,7 +47,7 @@ const albumsApi = createApi({
       }),
       removeAlbum: builder.mutation({
         invalidatesTags: (result, error, album) => {
-          return [{ type: "Album", id: album.userId }];
+          return [{ type: "Album", id: album.id }];
         },
         query: (album) => {
           return {
