cedc4f6f1f4f7ffa97ec61c8f31031a4d19f0214
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-30 09:40:06 -0700

    Add pauses to every http request so we could clearly see the spinners working in every feature

--- src/store/thunks/addUser.js
+++ src/store/thunks/addUser.js
@@ -6,7 +6,16 @@ const addUser = createAsyncThunk("users/add", async () => {
   const response = await axios.post("http://localhost:3005/users", {
     name: faker.name.fullName(),
   });
+  // Development Only!
+  await pause(1000);
   return response.data;
 });
+
+// Development Only!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { addUser };
--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -3,7 +3,16 @@ import axios from "axios";
 
 const removeUser = createAsyncThunk("users/remove", async (user) => {
   await axios.delete(`http://localhost:3005/users/${user.id}`);
+  // Development Only!
+  await pause(1000);
   return user;
 });
+
+// Development Only!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { removeUser };
