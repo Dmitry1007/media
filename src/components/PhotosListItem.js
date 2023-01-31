15569b3cb821ac176027c23e79a0ad8633d6721f
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-31 09:16:18 -0700

    Show a loading spinner while photo is being deleted

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,8 +1,8 @@
-import { GoTrashcan } from "react-icons/go";
+import { GoTrashcan, GoSync } from "react-icons/go";
 import { useRemovePhotoMutation } from "../store";
 
 function PhotosListItem({ photo }) {
-  const [removePhoto] = useRemovePhotoMutation(photo);
+  const [removePhoto, results] = useRemovePhotoMutation(photo);
 
   const handleClick = () => {
     removePhoto(photo);
@@ -12,7 +12,11 @@ function PhotosListItem({ photo }) {
     <div onClick={handleClick} className="relative cursor-pointer m-2">
       <img className="h-25 w-25" src={photo.url} alt="random pic" />
       <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
-        <GoTrashcan className="text-3xl" />
+        {results.isLoading ? (
+          <GoSync className="animate-spin text-3xl" />
+        ) : (
+          <GoTrashcan className="text-3xl" />
+        )}
       </div>
     </div>
   );
