fa3eece134dc4c4aafeddbe5333e926123e750c9
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-31 09:12:08 -0700

    Remove photo from db when user hovers over photo and clicks on trashcan

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -2,8 +2,14 @@ import { GoTrashcan } from "react-icons/go";
 import { useRemovePhotoMutation } from "../store";
 
 function PhotosListItem({ photo }) {
+  const [removePhoto] = useRemovePhotoMutation(photo);
+
+  const handleClick = () => {
+    removePhoto(photo);
+  };
+
   return (
-    <div className="relative cursor-pointer m-2">
+    <div onClick={handleClick} className="relative cursor-pointer m-2">
       <img className="h-25 w-25" src={photo.url} alt="random pic" />
       <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
         <GoTrashcan className="text-3xl" />
